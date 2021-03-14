8b50518f3767761a17a402f667d7a61401a29f6a
caokeyu <ckysaltedfish@163.com>
2021-03-14 19:39:46 +0800

    update

--- spring-demo/src/main/java/com/spring/mvc/config/AppConfig.java
+++ spring-demo/src/main/java/com/spring/mvc/config/AppConfig.java
@@ -1,13 +1,19 @@
 package com.spring.mvc.config;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.spring.mvc.handler.StaticResourceHandlerAdapter;
 import com.spring.mvc.handler.StaticResourceHandlerMapping;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.validation.MessageCodesResolver;
 import org.springframework.validation.Validator;
@@ -109,7 +115,32 @@ public class AppConfig implements WebMvcConfigurer  {
 //        }
         FastJsonHttpMessageConverter fastJsonHttpMessageConverter
                 = new FastJsonHttpMessageConverter();
-        converters.add(fastJsonHttpMessageConverter);
+		List<MediaType> supportedMediaTypes = new ArrayList<>();
+		supportedMediaTypes.add(MediaType.APPLICATION_JSON);
+		supportedMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+		supportedMediaTypes.add(MediaType.APPLICATION_ATOM_XML);
+		supportedMediaTypes.add(MediaType.APPLICATION_FORM_URLENCODED);
+		supportedMediaTypes.add(MediaType.APPLICATION_OCTET_STREAM);
+		supportedMediaTypes.add(MediaType.APPLICATION_PDF);
+		supportedMediaTypes.add(MediaType.APPLICATION_RSS_XML);
+		supportedMediaTypes.add(MediaType.APPLICATION_XHTML_XML);
+		supportedMediaTypes.add(MediaType.APPLICATION_XML);
+		supportedMediaTypes.add(MediaType.IMAGE_GIF);
+		supportedMediaTypes.add(MediaType.IMAGE_JPEG);
+		supportedMediaTypes.add(MediaType.IMAGE_PNG);
+		supportedMediaTypes.add(MediaType.TEXT_EVENT_STREAM);
+		supportedMediaTypes.add(MediaType.TEXT_HTML);
+		supportedMediaTypes.add(MediaType.TEXT_MARKDOWN);
+		supportedMediaTypes.add(MediaType.TEXT_PLAIN);
+		supportedMediaTypes.add(MediaType.TEXT_XML);
+		fastJsonHttpMessageConverter.setSupportedMediaTypes(supportedMediaTypes);
+		fastJsonHttpMessageConverter.setDefaultCharset(Charset.forName("UTF-8"));
+		FastJsonConfig config = new FastJsonConfig();
+		JSON.DEFFAULT_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+		config.setSerializerFeatures(SerializerFeature.WriteDateUseDateFormat);//格式化时间
+		fastJsonHttpMessageConverter.setFastJsonConfig(config);
+
+		converters.add(fastJsonHttpMessageConverter);
     }
 
     @Override
--- spring-demo/src/main/java/com/spring/mvc/cotroller/TestController.java
+++ spring-demo/src/main/java/com/spring/mvc/cotroller/TestController.java
@@ -1,14 +1,16 @@
 package com.spring.mvc.cotroller;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 public class TestController  {
@@ -21,10 +23,19 @@ public class TestController  {
         System.out.println("调用了");
         request.getParameter("name");
         Map hashMap = new HashMap();
-//        {}
         hashMap.put("key","value");
         return  hashMap;
     }
+
+	@PostMapping(value = "/test2.do")
+	@ResponseBody                                                   //user 对象  Map
+	public Object test2(@RequestBody Map<String, Object> param, HttpServletRequest request, HttpServletResponse response){
+		System.out.println("调用了");
+		Map<String, Object> resutlMap = new HashMap();
+		resutlMap.putAll(param);
+		resutlMap.put("date", new Date());
+		return  resutlMap;
+	}
 
     @RequestMapping("/model.do")
 //    @ResponseBody
